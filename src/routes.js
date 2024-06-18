6df656c1fcd51104c1cc8c178a67c083cb62f45c
TiagoJBO <timarao2@hotmail.com>
2024-06-18 10:30:24 -0300

    Corrigindo routes.js

--- src/routes.js
+++ src/routes.js
@@ -2,36 +2,36 @@ import { Router } from 'express'
 import multer from 'multer'
 import multerConfig from './config/multer'
 
-import UserController from './app/controllers/UserController'
+import UserController from './ap/controllers/UserController'
 
-import SessionController from './app/controllers/SessionController'
-import ProductController from './app/controllers/ProductController'
-import CategoryController from './app/controllers/CategoryController'
-import OrderController from './app/controllers/OrderController'
+import SessionController from './ap/controllers/SessionController'
+import productController from './ap/controllers/productController'
+import CategoryController from './ap/controllers/CategoryController'
+import OrderController from './ap/controllers/OrderController'
 
-import authMiddleware from './app/middlewares/auth'
+import authMiddleware from './ap/middlewares/auth'
 
 const uploads = multer(multerConfig)
 
 const routes = new Router()
 
-routes.get('/',(req, res)=>{
-    return res.json({message: 'Hello to my first API'})
+routes.get('/', (req, res) => {
+  return res.json({ message: 'Hello to my first ApI' })
 })
 
-routes.post('/Users', UserController.store)
+routes.post('/users', UserController.store)
 
-routes.post('/Sessions', SessionController.store)
+routes.post('/sessions', SessionController.store)
 
 routes.use(authMiddleware) // será chamados por todas as rotas ABAIXO
 
-routes.post('/Products', uploads.single('file'), ProductController.store)
-routes.get('/Products', ProductController.index)
-routes.put('/Products/:id', uploads.single('file'), ProductController.update)
+routes.post('/products', uploads.single('file'), productController.store)
+routes.get('/products', productController.index)
+routes.put('/products/:id', uploads.single('file'), productController.update)
 
-routes.post('/Categories', uploads.single('file'), CategoryController.store)
-routes.get('/Categories', CategoryController.index)
-routes.put('/Categories/:id', uploads.single('file'), CategoryController.update)
+routes.post('/categories', uploads.single('file'), CategoryController.store)
+routes.get('/categories', CategoryController.index)
+routes.put('/categories/:id', uploads.single('file'), CategoryController.update)
 
 routes.post('/orders', OrderController.store)
 routes.put('/orders/:id', OrderController.update)
