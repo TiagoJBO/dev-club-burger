9acd2a0ff2a184fdf8d95003f85ae7472aa4e927
TiagoJBO <timarao2@hotmail.com>
2024-06-18 10:48:02 -0300

    Corrigindo routes

--- src/routes.js
+++ src/routes.js
@@ -2,14 +2,14 @@ import { Router } from 'express'
 import multer from 'multer'
 import multerConfig from './config/multer'
 
-import UserController from './ap/controllers/UserController'
+import UserController from './app/controllers/UserController'
 
-import SessionController from './ap/controllers/SessionController'
-import productController from './ap/controllers/productController'
-import CategoryController from './ap/controllers/CategoryController'
-import OrderController from './ap/controllers/OrderController'
+import SessionController from './app/controllers/SessionController'
+import productController from './app/controllers/productController'
+import CategoryController from './app/controllers/CategoryController'
+import OrderController from './app/controllers/OrderController'
 
-import authMiddleware from './ap/middlewares/auth'
+import authMiddleware from './app/middlewares/auth'
 
 const uploads = multer(multerConfig)
 
@@ -25,7 +25,7 @@ routes.post('/sessions', SessionController.store)
 
 routes.use(authMiddleware) // será chamados por todas as rotas ABAIXO
 
-routes.post('/products', uploads.single('file'), productController.store)
+routes.post('/Products', uploads.single('file'), productController.store)
 routes.get('/products', productController.index)
 routes.put('/products/:id', uploads.single('file'), productController.update)
 
