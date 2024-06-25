004ac487ed2cf8fd11ae9375e701b451ae453f42
TiagoJBO <timarao2@hotmail.com>
2024-06-25 17:40:28 -0300

    Atualizando routes

--- src/routes.js
+++ src/routes.js
@@ -16,22 +16,22 @@ const uploads = multer(multerConfig)
 const routes = new Router()
 
 routes.get('/', (req, res) => {
-  return res.json({ message: 'Hello to my first API' })
+  return res.status(200).json({ message: 'Hello to my first API' })
 })
 
 routes.post('/users', UserController.store)
 
-routes.post('/Sessions', SessionController.store)
+routes.post('/sessions', SessionController.store)
 
 routes.use(authMiddleware) // será chamados por todas as rotas ABAIXO
 
-routes.post('/Products', uploads.single('file'), productController.store)
-routes.get('/Products', productController.index)
-routes.put('/Products/:id', uploads.single('file'), productController.update)
+routes.post('/products', uploads.single('file'), productController.store)
+routes.get('/products', productController.index)
+routes.put('/products/:id', uploads.single('file'), productController.update)
 
-routes.post('/Categories', uploads.single('file'), CategoryController.store)
-routes.get('/Categories', CategoryController.index)
-routes.put('/Categories/:id', uploads.single('file'), CategoryController.update)
+routes.post('/categories', uploads.single('file'), CategoryController.store)
+routes.get('/categories', CategoryController.index)
+routes.put('/categories/:id', uploads.single('file'), CategoryController.update)
 
 routes.post('/orders', OrderController.store)
 routes.put('/orders/:id', OrderController.update)
